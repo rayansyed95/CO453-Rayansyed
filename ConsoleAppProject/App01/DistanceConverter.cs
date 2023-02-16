107fd7d716ac482382c4e5a3f0dda620473dd69b
rayansyed95 <114736482+rayansyed95@users.noreply.github.com>
2023-02-16 11:17:56 +0000

    distanceconverter.cs

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -6,15 +6,58 @@ namespace ConsoleAppProject.App01
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Rayan version 0.1
     /// </author>
     public class DistanceConverter
     {
-        public void Run()
-    {
-        Console.WriteLine("Please enter the number of mixture.");
-        int miles = Convert.ToInt32(Console.ReadLine());
-    }
+        const int MILES_TO_FEET = 5280;
+        string input;
+        int miles;
+        int feet;
 
+        public DistanceConverter()
+        {
+
+        }
+        public void Run()
+        {
+            ConvertMilesToFeet();
+            ConvertFeetToMiles();
+        }
+
+        public void ConvertMilesToFeet()
+        {
+            //step 1 - input miles
+
+            Console.WriteLine("Please enter the number of miles: ");
+            input = Console.ReadLine();
+            miles = Convert.ToInt32(input);
+            Console.WriteLine("Miles entered is: " + miles);
+
+            // step 2 - conversion process 
+            
+            feet = miles* MILES_TO_FEET;
+
+            // step 3 - output converted number of feet
+
+            Console.WriteLine(miles + " miles is " + feet + " feet.");
+        }
+
+        public void ConvertFeetToMiles()
+        {
+
+            //step 1 - input feet
+
+            Console.WriteLine("Please enter the number of feet: ");
+            input = Console.ReadLine();
+            feet = Convert.ToInt32(input);
+            Console.WriteLine("Feet entered is: " + feet);
+
+            // step 2 - conversion process 
+            miles = feet / MILES_TO_FEET;
+
+            // step 3 - output converted number of miles
+            Console.WriteLine(feet + " feet is " + miles + " miles.");
+        }
     }
 }
