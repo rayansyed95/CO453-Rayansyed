91ef9795d91264b85a7abc32cec5db6e41abe0b5
rayansyed95 <114736482+rayansyed95@users.noreply.github.com>
2023-03-17 09:43:18 +0500

    Few changes for validating user input

--- ConsoleAppProject/App03/Program.cs
+++ ConsoleAppProject/App03/Program.cs
@@ -1,18 +1,15 @@
-﻿using System;
-
+using System;
 class Program
 {
     public Program()
     {
         throw new System.NotImplementedException();
     }
-
     static void Main(string[] args)
     {
         const int NUM_STUDENTS = 10;
         string[] studentNames = new string[NUM_STUDENTS];
         int[] studentMarks = new int[NUM_STUDENTS];
-
         Console.WriteLine("================================");
         Console.WriteLine("Students Marks Analysis Program");
         Console.WriteLine("    Developed by Rayan Syed");
@@ -22,10 +19,24 @@ class Program
         {
             Console.Write($"Enter name of student {i + 1}: ");
             studentNames[i] = Console.ReadLine();
-            Console.Write($"Enter marks of student {i + 1}: ");
-            studentMarks[i] = int.Parse(Console.ReadLine());
+            bool validMark = false;
+            while (!validMark)
+            {
+                Console.Write($"Enter marks of student {i + 1}: ");
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out studentMarks[i]))
+                {
+                    Console.WriteLine("Invalid input: Please enter a number");
+                    continue;
+                }
+                if (studentMarks[i] < 0 || studentMarks[i] > 100)
+                {
+                    Console.WriteLine("Invalid input: Please enter a number between 0 and 100");
+                    continue;
+                }
+                validMark = true;
+            }
         }
-
         // Convert marks to grades and save them
         string[] studentGrades = new string[NUM_STUDENTS];
         int[] gradeCounts = new int[5]; // 5 grades: A, B, C, D, Fail
@@ -57,7 +68,6 @@ class Program
                 gradeCounts[4]++;
             }
         }
-
         // Calculate statistics
         int totalMarks = 0;
         int minMarks = int.MaxValue;
@@ -75,7 +85,6 @@ class Program
             }
         }
         double avgMarks = (double)totalMarks / NUM_STUDENTS;
-
         // Print the student names, marks, and grades
         Console.WriteLine("\n===================================");
         Console.WriteLine("\nName\t\tMarks\t\tGrade\n");
@@ -84,7 +93,6 @@ class Program
         {
             Console.WriteLine($"{studentNames[i]}\t\t{studentMarks[i]}\t\t{studentGrades[i]}");
         }
-
         // Print the statistics
         Console.WriteLine("\n========================");
         Console.WriteLine("\nClass Marks Statistics");
@@ -92,7 +100,6 @@ class Program
         Console.WriteLine($"\nAverage Marks: {avgMarks:F2}");
         Console.WriteLine($"\nMinimum Marks: {minMarks}");
         Console.WriteLine($"\nMaximum Marks: {maxMarks}");
-
         // Print the grade-wise count and percentage of students
         Console.WriteLine("\n=====================================");
         Console.WriteLine("\nGrade-wise distribution of students:");
@@ -102,11 +109,9 @@ class Program
             double percentage = (double)gradeCounts[i] / NUM_STUDENTS * 100; //calculates % of each grade
             Console.WriteLine($"Grade {(char)('A' + i)}: {gradeCounts[i]} students ({percentage:F2}%)"); //this is used to print grade names
         }
-
         // Wait for user to press a key before exiting
         Console.ReadKey();
     }
-
     public void Method()
     {
         throw new System.NotImplementedException();
