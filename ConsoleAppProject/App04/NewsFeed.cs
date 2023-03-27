b9fce47f5462bf23208b3b4fc03c46306c2656ef
rayansyed95 <114736482+rayansyed95@users.noreply.github.com>
2023-03-27 09:14:03 +0500

    Update NewsFeed.cs

--- ConsoleAppProject/App04/NewsFeed.cs
+++ ConsoleAppProject/App04/NewsFeed.cs
@@ -15,7 +15,7 @@ namespace ConsoleAppProject.App04
     /// search or ordering functions.
     ///</summary>
     ///<author>
-    ///  Michael Kölling and David J. Barnes
+    ///  Michael Kölling and David J. Barnes - Updated by Rayan Syed
     ///  version 0.1
     ///</author> 
     public class NewsFeed
