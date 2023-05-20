676b41c1b55626b511893f272bc037a880a4dc5a
rayansyed95 <114736482+rayansyed95@users.noreply.github.com>
2023-05-20 20:05:30 +0500

    Fixed objects added

---
+++ ConsoleAppProject/App05/Rock.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rock here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rock extends Actor
+{
+    /**
+     * Act - do whatever the Rock wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Rock() 
+    {
+        setImage("rock.png"); // Set the image for the rock object
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ ConsoleAppProject/App05/Wall.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Wall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Wall extends Actor
+{
+    /**
+     * Act - do whatever the Wall wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Wall() 
+    {
+        setImage("wall.png"); // Set the image for the wall object
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
